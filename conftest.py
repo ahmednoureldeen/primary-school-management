72a1989ca6984fb2c882b892a40d695feb3208c0
ahmednoreldeen <ahmednoreldeen@gmail.com>
2025-02-14 17:37:26 +0200

    Use mock to avoid sending celery tasks while testing

--- conftest.py
+++ conftest.py
@@ -2,6 +2,10 @@ import pytest
 import django
 from django.conf import settings
 
+@pytest.fixture(autouse=True)
+def mock_celery_delay(mocker):
+    mocker.patch("celery.app.task.Task.delay", return_value=1)
+
 def pytest_configure():
     # Override only the database settings for testing
     settings.DATABASES = {
@@ -10,5 +14,6 @@ def pytest_configure():
             'NAME': 'test.db',
         }
     }
+    
 
     django.setup()
