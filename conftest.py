0272bb5e1a8ff1f51a2c72cbf749035654c2f963
ahmednoreldeen <ahmednoreldeen@gmail.com>
2025-02-15 13:28:45 +0200

    mock HomeworkDocument update and delete while testing

--- conftest.py
+++ conftest.py
@@ -6,6 +6,12 @@ from django.conf import settings
 def mock_celery_delay(mocker):
     mocker.patch("celery.app.task.Task.delay", return_value=1)
 
+
+@pytest.fixture(autouse=True)
+def mock_homework_document(mocker):
+    mocker.patch("homework.documents.HomeworkDocument.update", return_value=None)
+    mocker.patch("homework.documents.HomeworkDocument.delete", return_value=None)
+
 def pytest_configure():
     # Override only the database settings for testing
     settings.DATABASES = {
